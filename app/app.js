a63c9d0e46131b3d0ade6e9190287610c4af9056
AndrewKM210 <andrewkm210@gmail.com>
2019-08-26 19:03:23 +0200

    Server connected to contract

--- app/app.js
+++ app/app.js
@@ -1,4 +1,3 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -9,14 +8,19 @@ var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 var fs = require('fs');
 var obj = JSON.parse(fs.readFileSync('..//build/contracts/Coin.json', 'utf8'));
 
-// TODO: get contract address automatically
-var cnt = new web3.eth.Contract(obj.abi, "0x860caeB3033a1B47d54e8Ef742B3D607f57730D7");
+const contract_dir = "0x860caeB3033a1B47d54e8Ef742B3D607f57730D7";
+console.log("Contract address: " + contract_dir);
 
-cnt.methods.getAccount().call((err, result) => {console.log(result)});
+// TODO: get contract address automatically
+var contract = new web3.eth.Contract(obj.abi, contract_dir);
+
+web3.eth.getAccounts().then(function(accounts) {
+  web3.eth.defaultAccount = accounts[0];
+  console.log("Default account address: " + accounts[0]);
+});
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -26,9 +30,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '/')));
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('index', {
+    contract_address: contract_dir,
+    default_account: web3.eth.defaultAccount
+  });
 });
 
-app.listen(3000, function () {
+app.listen(3001, function () {
   console.log('App listening on port 3000.');
 });
