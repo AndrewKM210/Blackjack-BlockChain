3d21e4e7989d2cf2a30dda0e44bfff93bd7492c9
AndrewKM210 <andrewkm210@gmail.com>
2019-09-05 18:45:55 +0200

    Comments

--- app/app.js
+++ app/app.js
@@ -43,31 +43,66 @@ app.use(session({
   resave: false
 }));
 
-
-app.get('/', function(req, res) {
-  res.render('index', {
-    contract_address: contract_dir,
-    default_account: web3.eth.defaultAccount
-  });
-});
-
+/**
+ * Leaves the app listening on port 3000.
+ */
 app.listen(3000, function() {
   console.log('App listening on port 3000.');
 });
 
-app.get('/login', function (req, res) {
-  res.render('login');
+/**
+ * Renders the index page.
+ */
+app.get('/', function(req, res) {
+  res.render('index', {
+    contract_address: coinContract.address  
+  });
 });
 
+/**  
+ * Called when clicking the login button of the index page. Redirects to the
+ * login page
+ */
+app.post('/goLogin', function(req, res) {
+  res.redirect('login');
+})
+
+/**  
+ * Called when clicking the register button of the index page. Redirects to the
+ * register page
+ */
+app.post('/goRegister', function(req, res) {
+  res.redirect('register');
+})
+
+/**
+ * Renders the login page.
+ */
+app.get('/login', function (req, res) {
+  res.render('login', {
+    message: null
+  });
+});
+
+/**
+ * Called when clicking the login button. Checks that the address is valid and
+ * then tries to unlock the account. If succesful, it will render the game page.
+ * If not, it will return an error message.
+ */
 app.post('/login', function(req, res) {
+  
+  // Obtain account and password from form body.
   account = req.body.account;
   password = req.body.password;
   console.log('\n\nUNLOCKING ACCOUNT ' + account);
+
+  // Check if the address is valid
   if (web3.utils.isAddress(account)) {
-    web3.eth.personal.unlockAccount(account, password, 0).then(function(ok, error) {
-      if (error != null) {
-        console.log('Error unlocking account: ' + error);
-      } else {
+
+    // Try to unlock account
+      web3.eth.personal.unlockAccount(account, password, 0).then(function() {
+
+        // Render the game page
         console.log('Account ' + account + ' unlocked!');
         req.session.account = account;
         res.render('game', {
@@ -78,20 +113,36 @@ app.post('/login', function(req, res) {
           botStayed: false,
           message: 'Start by pressing the deal button'
         });
-      }
-    });
+      }).catch(function(error) {
+
+        // If an error is caught, return an error message
+        console.log('Error unlocking account: ' + error);
+        res.render('login', {
+          message: "Password is incorrect."
+        });
+      });
   } else {
+
+    // If the account is not valid, return an error message
     console.log('Error: account is not a valid ethereum address')
     res.render('login', {
-      error: 'Account is not a valid ethereum address'
+      message: 'Account is not a valid ethereum address.'
     });
   }
 });
 
+/**
+ * Renders the register page.
+ */
 app.get('/register', function(req, res) {
   res.render('register');
 })
 
+/**
+ * Called when the register button is clicked. It checks if both passwords
+ * typed in the form coincide. If they do, it creates a new account. If not, 
+ * shows an error message.
+ */
 app.post('/register', function(req, res) {
   console.log('\n\nCREATING NEW ACCOUNT');
   password = req.body.password;
@@ -107,27 +158,43 @@ app.post('/register', function(req, res) {
       })
     })
   } else {
-    console.log('Error: the passwords are not the same');
+    console.log('Error: the passwords don\'t coincide');
     res.render('register', {
-      error: 'The passwords are not the same'
+      message: 'The passwords don\'t coincide,'
     });
   }
 })
 
+/**
+ * Renders the game page.
+ */
 app.get('/game', function(req, res) {
   res.render('game', {
     cards: undefined,
     deal: true,
-    userTurn: false
+    userTurn: false,
+    message: null
   });
 })
 
+/**
+ * Called when the deal button is clicked, at the start of a new game. It 
+ * creates a shuffled pack deque of cards, and gives two to the user and two to
+ * the bot. It saves all the objects (user cards, bot cards, deque and scores)
+ * in the session variable.
+ */
 app.post('/deal', function(req, res) {
+
+  // Create new shuffled deque of cards
   var deque = new Deque();
+
+  // Give 2 cards to user and another 2 to the bot
   var firstCards = [deque.firstCard()];
   var firstCardsBot = [deque.firstCard()];
   firstCards.push(deque.firstCard());
   firstCardsBot.push(deque.firstCard());
+
+  // Save values in session
   req.session.deque = deque;
   req.session.cards = firstCards;
   req.session.botCards = firstCardsBot;
@@ -135,22 +202,37 @@ app.post('/deal', function(req, res) {
   req.session.botScore = firstCardsBot[0].number + firstCardsBot[1].number;
   req.session.stayed = false;
   req.session.botStayed = false;
+
+  // Render the game page again.
   res.render('game', {
     cards: firstCards,
     deal: false,
     userTurn: true,
-    stayed: false,
-    botStayed: false,
     message: 'Dealt the cards, you start.'
   })
 })
 
+/**
+ * Called when the play button is clicked. Gets the first card from the deque.
+ * If the value of the card in addition to the current score is lower or equal
+ * to 21, then it renders the page again. If not, it ends the game.
+ */
 app.post('/play', function(req, res) {
+
+  // Recuperate deque from session
   var deque = Object.assign(new Deque, req.session.deque);
+
+  // Get the first card
   var card = deque.firstCard();
   req.session.cards.push(card);
+
+  // Calculate new score
   req.session.score += card.number;
+
   if (req.session.score <= 21) {
+
+    // If the new score is under or equal 21 and the bot stayed last turn, it's
+    // still the user's turn
     if (req.session.botStayed) {
       res.render('game', {
         cards: req.session.cards,
@@ -158,6 +240,8 @@ app.post('/play', function(req, res) {
         userTurn: true,
         message: 'You have chosen to play, your turn.',
       });
+
+    // If the bot didn't stay, it's the bot's turn
     } else {
       res.render('game', {
         cards: req.session.cards,
@@ -167,6 +251,8 @@ app.post('/play', function(req, res) {
       });
     }
   } else {
+    // If the new score is over 21, the user has lost. It finishes the game, 
+    // showing the scores.
     console.log('User has surpassed 21 points');
     res.render('game', {
       cards: undefined,
@@ -177,77 +263,16 @@ app.post('/play', function(req, res) {
   }
 })
 
-app.post('/goLogin', function(req, res) {
-  res.redirect('login');
-})
-
-app.post('/goRegister', function(req, res) {
-  res.redirect('register');
-})
-
-app.post('/botTurn', function(req, res) {
-  if (req.session.botScore < 17) {
-    var deque = Object.assign(new Deque(), req.session.queue);
-    var newCard = deque.firstCard();
-    req.session.botScore += newCard.number;
-    req.session.botCards.push(newCard);
-    if (req.session.botScore <= 21) {
-      if (!req.session.stayed) {
-        res.render('game', {
-          cards: req.session.cards,
-          deal: false,
-          userTurn: true,
-          message: 'Bot has chosen to play, your turn.',
-        });
-      } else {
-        res.render('game', {
-          cards: req.session.cards,
-          deal: false,
-          userTurn: false,
-          message: 'Bot has chosen to play, bot\'s turn.',
-        })
-      }
-    } else {
-      res.render('game', {
-        cards: req.session.cards,
-        deal: true,
-        userTurn: false,
-        message: 'Bot has exceeded 21 points, you win.',
-      })
-    }
-  } else {
-    req.session.botStayed = true;
-    var message;
-    if (req.session.stayed) {
-      message = 'You have both chosen to stay. Your score: ' + req.session.score + '   Bot score: ' + req.session.botScore;
-      if (req.session.score > req.session.botScore) {
-        message += '. You win!';
-      } else if (req.session.score < req.session.botScore) {
-        message += '. You loose!';
-      } else {
-        message += '. It\'s a draw!';
-      }
-      res.render('game', {
-        cards: req.session.cards,
-        deal: true,
-        userTurn: false,
-        message: message,
-      });
-    } else {
-      message = 'Bot has chosen to stay, your turn.';
-      res.render('game', {
-        cards: req.session.cards,
-        deal: false,
-        userTurn: true,
-        message: message,
-      })
-    }
-  }
-})
-
+/**
+ * Called when clicking the stay button. The player doesn't lift a new card and
+ * stops playing until the bot decides to stay. If the bot has already decided
+ * to stay, the game ends.
+ */
 app.post('/stay', function(req, res) {
   req.session.stayed = true;
   var message;
+
+  // If bot has stayed, the game finishes.
   if (req.session.botStayed) {
     message = 'You have both chosen to stay. Your score: ' + req.session.score + '   Bot score: ' + req.session.botScore;
     if (req.session.score > req.session.botScore) {
@@ -263,6 +288,8 @@ app.post('/stay', function(req, res) {
       userTurn: false,
       message: message,
     });
+
+  // If bot hasn't finished, then the user has to wait until bot has finished.
   } else {
     message = 'You have chosen to stay, bot\'s turn.'
     res.render('game', {
@@ -273,3 +300,90 @@ app.post('/stay', function(req, res) {
     });
   }
 });
+
+/**
+ * Called when the Bot turn button is clicked. It simulates a players turn. If
+ * his score is under 17, it plays in a similar way to the user. If not, it 
+ * stays.
+ */
+app.post('/botTurn', function(req, res) {
+
+  // If its score is under 17
+  if (req.session.botScore < 17) {
+
+    // Recuperates the deque from the session variable
+    var deque = Object.assign(new Deque(), req.session.queue);
+
+    // Obtains the first card from the deque
+    var newCard = deque.firstCard();
+    req.session.botCards.push(newCard);
+
+    // Calculates the new score
+    req.session.botScore += newCard.number;
+  
+    // If the score is under or equal to 21
+    if (req.session.botScore <= 21) {
+
+      // If the user hasn't stayed, it's the user's turn
+      if (!req.session.stayed) {
+        res.render('game', {
+          cards: req.session.cards,
+          deal: false,
+          userTurn: true,
+          message: 'Bot has chosen to play, your turn.',
+        });
+      
+      // If the user has stayed, it's the bot's turn again
+      } else {
+        res.render('game', {
+          cards: req.session.cards,
+          deal: false,
+          userTurn: false,
+          message: 'Bot has chosen to play, bot\'s turn.',
+        })
+      }
+    // If the score is over 21, the bot has lost.
+    } else {
+      res.render('game', {
+        cards: req.session.cards,
+        deal: true,
+        userTurn: false,
+        message: 'Bot has exceeded 21 points, you win.',
+      })
+    }
+
+  // If the score is over 17, the bot stays
+  } else {
+    req.session.botStayed = true;
+    var message;
+
+    // If the user has stayed then the game has finished
+    if (req.session.stayed) {
+      message = 'You have both chosen to stay. Your score: ' + req.session.score + '   Bot score: ' + req.session.botScore;
+      if (req.session.score > req.session.botScore) {
+        message += '. You win!';
+      } else if (req.session.score < req.session.botScore) {
+        message += '. You loose!';
+      } else {
+        message += '. It\'s a draw!';
+      }
+      res.render('game', {
+        cards: req.session.cards,
+        deal: true,
+        userTurn: false,
+        message: message,
+      });
+
+    // If the user hasn't stayed, the bot has to wait until the user has 
+    // finished.
+    } else {
+      message = 'Bot has chosen to stay, your turn.';
+      res.render('game', {
+        cards: req.session.cards,
+        deal: false,
+        userTurn: true,
+        message: message,
+      })
+    }
+  }
+})
